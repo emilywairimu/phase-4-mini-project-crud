34ae01976bb1725be4d628087c30710783549d06
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-06-24 15:58:05 -0500

    done

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,47 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+    # GET /spices
+    def index 
+      spices = Spice.all 
+      render json: spices
+    end
+  
+    # POST /spices
+    def create 
+      spice = Spice.create(spice_params)
+      render json: spice, status: :created
+    end
+  
+    # PATCH /spices/:id
+    def update
+      spice = find_spice
+      if spice.update(spice_params)
+        render json: spice
+      else
+        render json: spice.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /spices/:id
+    def destroy
+      spice = find_spice
+      spice.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def find_spice
+      Spice.find_by(id: params[:id])
+    end
+  
+    def spice_params
+      params.permit(:title, :image, :description, :notes, :rating)
+    end
+  
+    def render_not_found_response
+      render json: { error: "Spice not found" }, status: :not_found
+    end
+  end
+  
